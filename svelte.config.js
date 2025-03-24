8e675b161d31b6e30de6ca2567cb9012757b838d
sean <seanluse41@gmail.com>
2025-03-24 13:44:23 +0900

    commit and test build

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,22 @@
-// Tauri doesn't have a Node.js server to do proper SSR
-// so we will use adapter-static to prerender the app (SSG)
-// See: https://v2.tauri.app/start/frontend/sveltekit/ for more info
-
+// svelte.config.js (existing code with additions)
 import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import { readFileSync } from 'fs';
+import { fileURLToPath } from 'url';
+
+const file = fileURLToPath(new URL('package.json', import.meta.url));
+const json = readFileSync(file, 'utf8');
+const pkg = JSON.parse(json);
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
     adapter: adapter(),
+    version: {
+      name: pkg.version
+    }
   },
   preprocess: [vitePreprocess({})],
 };
-
 
 export default config;
